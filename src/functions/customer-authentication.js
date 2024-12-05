a025534fdfc840925f19f1e27610d0bf489bf67a
Alexandre Nascimento <alexandre.n@outlook.com>
2024-12-04 21:19:15 -0300

    Add customer authentication function using Azure
    Implemented a new HTTP POST function for customer authentication based on CPF validation. Connected to a PostgreSQL database to verify customer existence, returning appropriate responses for success, invalid CPF, or nonexistent users. Added error handling and logging for request processing steps.

---
+++ src/functions/customer-authentication.js
@@ -0,0 +1,71 @@
+const {app} = require('@azure/functions');
+const {Pool} = require('pg');
+const {cpf: cpfValidator} = require('cpf-cnpj-validator');
+
+app.http('customer-authentication', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        // Log de início da requisição
+        context.info('Iniciando autenticação de cliente.');
+
+        // Obtendo o CPF do corpo da requisição
+        const {cpf} = await request.json();
+        context.info(`CPF recebido para autenticação: ${cpf}`);
+
+        // Validação do CPF
+        if (!cpfValidator.isValid(cpf)) {
+            context.error(`CPF inválido: ${cpf}`);
+            return {
+                status: 400,
+                body: JSON.stringify({message: 'CPF inválido'}),
+            };
+        }
+
+        // Conectando ao banco de dados
+        const pool = new Pool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+        });
+        context.info('Tentando conexão ao banco de dados...');
+
+        const client = await pool.connect();
+        context.info('Conexão ao banco de dados realizada com sucesso.')
+
+        try {
+            // Executando consulta SQL
+            context.info(`Executando consulta no banco para CPF: ${cpf}`);
+            const query = 'SELECT * FROM "Customers" WHERE "Cpf" = $1';
+            const res = await client.query(query, [cpf]);
+
+            if (res.rows.length === 0) {
+                context.error(`Nenhum usuário encontrado com o CPF: ${cpf}`);
+                return {
+                    status: 401,
+                    body: JSON.stringify({message: 'Usuário não encontrado'}),
+                };
+            }
+            // Usuário encontrado
+            const customer = res.rows[0];
+            context.info(`Usuário encontrado com sucesso: ${JSON.stringify(customer)}`);
+
+            return {
+                statusCode: 200,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({customer}),
+            };
+
+        } catch (error) {
+            // Tratamento de erro e log do erro
+            context.error('Erro durante o processamento da requisição:', error);
+            throw error;
+        } finally {
+            client.release();
+        }
+    }
+});
